019f4caf03bc3246dff2c9f83667626e0f5594a1
Roshan Rateria <93132440+roshanrateria@users.noreply.github.com>
2025-02-12 21:14:54 +0530

    Delete survey_portal directory

--- survey_portal/survey_portal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from surveys import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),  
-    path('', include('surveys.urls')),
-]
